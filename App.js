5d3d720355a3cb7c2e63bf90c8b8644b607e33bd
mollyisaac <54511777+mollyisaac@users.noreply.github.com>
2021-02-17 10:01:01 -0600

    working on user input stuff

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
-
-function MyComponent({ title, onIncrement }) {
+//Counter
+/*function MyComponent({ title, onIncrement }) {
   return <Button title={title} onPress={onIncrement} />
 }
 
@@ -19,26 +19,56 @@ export default function Counter() {
 
     />
   )
+}*/
+
+//Conditional
+/*const Card = ({ loading, error, title }) => {
+  let content
+  if (error) {
+    content = <Text style={{ fontSize: 24, color: 'red' }}>Error</Text>
+  }
+  else if (loading) {
+    content = <Text style={{ fontSize: 24, color: 'gray' }}>Loading...</Text>
+  }
+  else {
+    content = (
+      <View>
+        <Text style={{ fontSize: 60 }}>{title}</Text>
+      </View>
+    )
+  }
+  return <View style={{ padding: 24 }}>{content}</View>
 }
 
-/*export default function App() {
+export default function App() {
   return (
-
-    <Text>
-      Counter
-    </Text>
-
-    /*<View>
-
-      <MyComponent title="Press Me" color="#1ACDA5" />
-      <MyComponent title="No, press me!" color="rgb(59, 108, 212)" />
+    <View>
+      <Card error={true} />
+      <Card loading={true} />
+      <Card loading={false} title="Title" />
     </View>
-    // <View style={styles.container}>
-    // <Text>Hey there!</Text>
-    //<StatusBar style="auto" />
-    //</View>
   )
 }*/
+
+//useState hook
+export default function App() {
+  const [text, setText] = useState('')
+  return (
+    <View>
+      <TextInput
+        value={text}
+        style={{ fontSize: 42, color: 'steelblue' }}
+        placeholder="Typehere..."
+        onChangeText={(text) => {
+          setText(text)
+        }}
+      />
+      <Text style={{ fontSize: 24 }}>
+        {'\n'}You entered: {text}
+      </Text>
+    </View>
+  )
+}
 
 const styles = StyleSheet.create({
   container: {

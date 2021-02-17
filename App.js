c44d97f7e9a8e8c5977b3ccb17b694af74b5b865
mollyisaac <54511777+mollyisaac@users.noreply.github.com>
2021-02-16 22:02:39 -0600

    working on buttons and counters

--- App.js
+++ App.js
@@ -1,15 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hey there!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+function MyComponent({ title, onIncrement }) {
+  return <Button title={title} onPress={onIncrement} />
 }
+
+
+
+export default function Counter() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <MyComponent
+      title={`Click here to increment: ${count}`}
+      onIncrement={() => setCount(count + 1)}
+
+    />
+  )
+}
+
+/*export default function App() {
+  return (
+
+    <Text>
+      Counter
+    </Text>
+
+    /*<View>
+
+      <MyComponent title="Press Me" color="#1ACDA5" />
+      <MyComponent title="No, press me!" color="rgb(59, 108, 212)" />
+    </View>
+    // <View style={styles.container}>
+    // <Text>Hey there!</Text>
+    //<StatusBar style="auto" />
+    //</View>
+  )
+}*/
 
 const styles = StyleSheet.create({
   container: {
